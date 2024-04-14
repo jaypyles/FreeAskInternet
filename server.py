67c5995bf4103862c979cefe2983aea3e4bf1845
Jayden Pyles <jpylesbuisness@gmail.com>
2024-04-14 16:37:04 -0500

    fix: allow english use

--- server.py
+++ server.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import time
-import uvicorn
-import sys
+import concurrent
 import getopt
 import json
-import os 
+import os
+import platform
+import sys
+import time
+import urllib.parse
+from concurrent.futures import ThreadPoolExecutor
+from contextlib import asynccontextmanager
+from datetime import datetime
 from pprint import pprint
+from typing import Any, Dict, List, Literal, Optional, Union
+from urllib.parse import urlparse
+
+import openai
 import requests
 import trafilatura
-from trafilatura import bare_extraction
-from concurrent.futures import ThreadPoolExecutor
-import concurrent
-import requests
-import openai
-import time 
-from datetime import datetime
-from urllib.parse import urlparse
-import platform
-import urllib.parse
-import free_ask_internet
-from pydantic import BaseModel, Field
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-from typing import Any, Dict, List, Literal, Optional, Union
-from sse_starlette.sse import ServerSentEvent, EventSourceResponse
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel, Field
+from sse_starlette.sse import EventSourceResponse, ServerSentEvent
+from trafilatura import bare_extraction
+
+import free_ask_internet
 
 app = FastAPI()
 
@@ -65,13 +65,14 @@ class DeltaMessage(BaseModel):
 
 
 class QueryRequest(BaseModel):
-    query:str
+    query: str
     model: str
-    ask_type:  Literal["search", "llm"]
+    ask_type: Literal["search", "llm"]
     llm_auth_token: Optional[str] = "CUSTOM"
     llm_base_url: Optional[str] = ""
-    using_custom_llm:Optional[bool] = False
-    lang:Optional[str] = "zh-CN"
+    using_custom_llm: Optional[bool] = False
+    lang: Optional[str] = "zh-CN"
+
 
 class ChatCompletionRequest(BaseModel):
     model: str
@@ -97,25 +98,29 @@ class ChatCompletionResponseStreamChoice(BaseModel):
 class ChatCompletionResponse(BaseModel):
     model: str
     object: Literal["chat.completion", "chat.completion.chunk"]
-    choices: List[Union[ChatCompletionResponseChoice,
-                        ChatCompletionResponseStreamChoice]]
+    choices: List[
+        Union[ChatCompletionResponseChoice, ChatCompletionResponseStreamChoice]
+    ]
     created: Optional[int] = Field(default_factory=lambda: int(time.time()))
+
 
 class SearchItem(BaseModel):
     url: str
     icon_url: str
-    site_name:str
-    snippet:str
-    title:str 
+    site_name: str
+    snippet: str
+    title: str
+
 
 class SearchItemList(BaseModel):
     search_items: List[SearchItem] = []
- 
+
+
 class SearchResp(BaseModel):
-    code:int
-    msg:str
+    code: int
+    msg: str
     data: List[SearchItem] = []
- 
+
 
 @app.get("/v1/models", response_model=ModelList)
 async def list_models():
@@ -132,25 +137,22 @@ async def create_chat_completion(request: ChatCompletionRequest):
         raise HTTPException(status_code=400, detail="Invalid request")
     query = request.messages[-1].content
 
-
     generate = predict(query, "", request.model)
     return EventSourceResponse(generate, media_type="text/event-stream")
 
- 
 
 def predict(query: str, history: None, model_id: str):
     choice_data = ChatCompletionResponseStreamChoice(
-        index=0,
-        delta=DeltaMessage(role="assistant"),
-        finish_reason=None
+        index=0, delta=DeltaMessage(role="assistant"), finish_reason=None
     )
-    chunk = ChatCompletionResponse(model=model_id, choices=[
-                                choice_data], object="chat.completion.chunk")
+    chunk = ChatCompletionResponse(
+        model=model_id, choices=[choice_data], object="chat.completion.chunk"
+    )
     yield "{}".format(chunk.json(exclude_unset=True))
     new_response = ""
     current_length = 0
     for token in free_ask_internet.ask_internet(query=query):
-    
+
         new_response += token
         if len(new_response) == current_length:
             continue
@@ -160,73 +162,96 @@ def predict(query: str, history: None, model_id: str):
 
         choice_data = ChatCompletionResponseStreamChoice(
             index=0,
-            delta=DeltaMessage(content=new_text,role="assistant"),
-            finish_reason=None
+            delta=DeltaMessage(content=new_text, role="assistant"),
+            finish_reason=None,
         )
-        chunk = ChatCompletionResponse(model=model_id, choices=[
-                                       choice_data], object="chat.completion.chunk")
+        chunk = ChatCompletionResponse(
+            model=model_id, choices=[choice_data], object="chat.completion.chunk"
+        )
         yield "{}".format(chunk.json(exclude_unset=True))
 
     choice_data = ChatCompletionResponseStreamChoice(
-        index=0,
-        delta=DeltaMessage(),
-        finish_reason="stop"
+        index=0, delta=DeltaMessage(), finish_reason="stop"
     )
-    chunk = ChatCompletionResponse(model=model_id, choices=[
-                                   choice_data], object="chat.completion.chunk")
+    chunk = ChatCompletionResponse(
+        model=model_id, choices=[choice_data], object="chat.completion.chunk"
+    )
     yield "{}".format(chunk.json(exclude_unset=True))
-    yield '[DONE]'
- 
+    yield "[DONE]"
 
 
 @app.post("/api/search/get_search_refs", response_model=SearchResp)
 async def get_search_refs(request: QueryRequest):
-
     global search_results
     search_results = []
     search_item_list = []
     if request.ask_type == "search":
-        search_links,search_results = free_ask_internet.search_web_ref(request.query)
+        search_links, search_results = free_ask_internet.search_web_ref(request.query)
         for search_item in search_links:
             snippet = search_item.get("snippet")
             url = search_item.get("url")
             icon_url = search_item.get("icon_url")
             site_name = search_item.get("site_name")
             title = search_item.get("title")
-    
 
-            si = SearchItem(snippet=snippet,url=url,icon_url=icon_url,site_name=site_name,title=title)
+            si = SearchItem(
+                snippet=snippet,
+                url=url,
+                icon_url=icon_url,
+                site_name=site_name,
+                title=title,
+            )
 
             search_item_list.append(si)
 
-    resp = SearchResp(code=0,msg="success",data=search_item_list)
-   
-    return  resp
+    resp = SearchResp(code=0, msg="success", data=search_item_list)
 
-def generator(prompt:str, model:str, llm_auth_token:str,llm_base_url:str, using_custom_llm=False,is_failed=False):
+    return resp
+
+
+def generator(
+    prompt: str,
+    model: str,
+    llm_auth_token: str,
+    llm_base_url: str,
+    using_custom_llm=False,
+    is_failed=False,
+):
     if is_failed:
         yield "搜索失败，没有返回结果"
     else:
         total_token = ""
-        for token in  free_ask_internet.chat(prompt=prompt,model=model,llm_auth_token=llm_auth_token,llm_base_url=llm_base_url,using_custom_llm=using_custom_llm,stream=True):
+        for token in free_ask_internet.chat(
+            prompt=prompt,
+            model=model,
+            llm_auth_token=llm_auth_token,
+            llm_base_url=llm_base_url,
+            using_custom_llm=using_custom_llm,
+            stream=True,
+        ):
             total_token += token
             yield token
- 
+
+
 @app.post("/api/search/stream/{search_uuid}")
-async def stream(search_uuid:str,request: QueryRequest):
+async def stream(search_uuid: str, request: QueryRequest):
     global search_results
 
     if request.ask_type == "llm":
-            
-        answer_language = ' Simplified Chinese '
-        if request.lang == "zh-CN":
-            answer_language = ' Simplified Chinese '
-        if request.lang == "zh-TW":
-            answer_language = ' Traditional Chinese '
-        if request.lang == "en-US":
-            answer_language = ' English '
-        prompt = ' You are a large language AI assistant develop by nash_su. Answer user question in ' + answer_language + '. And here is the user question: ' + request.query
-        generate = generator(prompt,model=request.model,llm_auth_token=request.llm_auth_token, llm_base_url=request.llm_base_url, using_custom_llm=request.using_custom_llm)
+        answer_language = " English "
+        prompt = (
+            " You are a large language AI assistant develop by nash_su. Answer user question in "
+            + answer_language
+            + ". And here is the user question: "
+            + request.query
+        )
+        generate = generator(
+            prompt,
+            model=request.model,
+            llm_auth_token=request.llm_auth_token,
+            llm_base_url=request.llm_base_url,
+            using_custom_llm=request.using_custom_llm,
+        )
     else:
         prompt = None
         limit_count = 10
@@ -234,7 +259,12 @@ async def stream(search_uuid:str,request: QueryRequest):
         while limit_count > 0:
             try:
                 if len(search_results) > 0:
-                    prompt = free_ask_internet.gen_prompt(request.query,search_results,lang=request.lang,context_length_limit=8000)
+                    prompt = free_ask_internet.gen_prompt(
+                        request.query,
+                        search_results,
+                        lang=request.lang,
+                        context_length_limit=8000,
+                    )
                     break
                 else:
                     limit_count -= 1
@@ -242,23 +272,36 @@ async def stream(search_uuid:str,request: QueryRequest):
             except Exception as err:
                 limit_count -= 1
                 time.sleep(1)
-        total_token =  ""
-        if prompt:   
-            generate = generator(prompt,model=request.model,llm_auth_token=request.llm_auth_token, llm_base_url=request.llm_base_url, using_custom_llm=request.using_custom_llm)
+        total_token = ""
+        if prompt:
+            generate = generator(
+                prompt,
+                model=request.model,
+                llm_auth_token=request.llm_auth_token,
+                llm_base_url=request.llm_base_url,
+                using_custom_llm=request.using_custom_llm,
+            )
         else:
-            generate = generator(prompt,model=request.model,llm_auth_token=request.llm_auth_token,llm_base_url=request.llm_base_url, using_custom_llm=request.using_custom_llm,is_failed=True)
+            generate = generator(
+                prompt,
+                model=request.model,
+                llm_auth_token=request.llm_auth_token,
+                llm_base_url=request.llm_base_url,
+                using_custom_llm=request.using_custom_llm,
+                is_failed=True,
+            )
 
     # return EventSourceResponse(generate, media_type="text/event-stream")
     return StreamingResponse(generate, media_type="text/event-stream")
+
 
 def main():
 
     port = 8000
 
     search_results = []
- 
-   
-    uvicorn.run(app, host='0.0.0.0', port=port, workers=1)
+
+    uvicorn.run(app, host="0.0.0.0", port=port, workers=1)
 
 
 if __name__ == "__main__":
